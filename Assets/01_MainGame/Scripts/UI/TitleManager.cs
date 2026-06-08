5e58ef804a8a6a5a3a506e996d2deebc348a3d32
Sammy <kouyou.sbudiman@gmail.com>
2026-06-08 10:30:08 +0900

    色々更新した

--- Assets/01_MainGame/Scripts/UI/TitleManager.cs
+++ Assets/01_MainGame/Scripts/UI/TitleManager.cs
@@ -2,8 +2,11 @@ using UnityEngine;
 
 public class TitleManager : MonoBehaviour
 {
+    [SerializeField] private GameObject titleUI; // タイトル画面のルートUI（Canvas or Panel）
+
     public void OnStartButton()
     {
+        titleUI?.SetActive(false);
         GameManager.Instance.LoadGameScene();
     }
 }
